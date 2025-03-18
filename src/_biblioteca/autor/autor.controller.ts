74dd637ddd37fb7b453d110ec337a24cfb646188
migatomax123 <kevin.acupar18@gmail.com>
2025-03-18 20:54:45 +0100

    autenticacion en autores

--- src/_biblioteca/autor/autor.controller.ts
+++ src/_biblioteca/autor/autor.controller.ts
@@ -1,21 +1,40 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards } from '@nestjs/common';
 import { AutorService } from './autor.service';
 import { CreateAutorDto } from './dto/create-autor.dto';
 import { UpdateAutorDto } from './dto/update-autor.dto';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
 
 @Controller("autor") 
 export class AutorController {
   constructor(private readonly autorService: AutorService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createAutorDto: CreateAutorDto) {    return this.autorService.create(createAutorDto);  }
+  create(@Body() createAutorDto: CreateAutorDto) {
+    return this.autorService.create(createAutorDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {   return this.autorService.findAll();   }
+  findAll() {
+    return this.autorService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {    return this.autorService.findOne(+id);  }
+  findOne(@Param('id') id: string) {
+    return this.autorService.findOne(+id);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Put(":id")
   update(@Param('id') id: string, @Body() updateAutorDto: UpdateAutorDto) {
-    return this.autorService.update(+id, updateAutorDto)
+    return this.autorService.update(+id, updateAutorDto);
   }
+
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {  return this.autorService.remove(+id);  }
+  remove(@Param('id') id: string) {
+    return this.autorService.remove(+id);
+  }
 }
